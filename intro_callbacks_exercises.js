7992a27384d211079880c7d3ad09215f06f7ee02
jegalchoi <thefreshbegal@gmail.com>
2019-11-13 12:41:33 -0800

    Complete Timing is Everything: addNumbers

--- intro_callbacks_exercises.js
+++ intro_callbacks_exercises.js
@@ -20,3 +20,28 @@ class Clock {
 
 //const clock = new Clock();
 
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+  if (numsLeft > 0) {
+    reader.question("Number? ", function (number) {
+      const Num = parseInt(number);
+
+      sum += Num;
+      console.log(`${sum}`);
+
+      addNumbers(sum, numsLeft - 1, completionCallback);
+    })
+  } else if (numsLeft === 0) {
+    completionCallback(sum);
+    reader.close();
+  };
+}
+
+//addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
