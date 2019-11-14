e2c5b429bd5bbb5b0f8413d3626b5bd518ad20ce
jegalchoi <thefreshbegal@gmail.com>
2019-11-14 11:17:03 -0800

    Complete myThrottle

--- intro_callbacks_exercises.js
+++ intro_callbacks_exercises.js
@@ -135,3 +135,37 @@ Function.prototype.myBind = function(context) {
 
 //boundTurnOn();
 //myBoundTurnOn();
+
+Function.prototype.myThrottle = function(interval) {
+  let tooSoon = false;
+
+  return () => {
+    if (tooSoon == false) {
+      tooSoon = true;
+
+      setTimeout(function() {
+        tooSoon = false;
+      }, interval);
+
+      this();
+    }
+  }
+}
+
+class Neuron {
+  fire() {
+    console.log("Firing!")
+  }
+}
+
+/*
+const neuron = new Neuron();
+
+neuron.fire = neuron.fire.myThrottle(3000);
+
+const interval = setInterval(() => {
+  neuron.fire();
+}, 10);
+*/
+
+
